46a776399c1a43b86d1f317899e5eaa32638daee
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-12-04 19:46:47 +0100

    remove useless scripts

--- scripts/kafka/initTopics.sh
+++ scripts/kafka/initTopics.sh
@@ -5,9 +5,6 @@ set -Eeu
 
 # "source .env" does not work when it contains string array
 # As a temporary fix we hard-paste the values we need from .env here.
-# TODO: Have clean configuration
-ETH_CLIENT_URL_FROM_LOCALHOST="http://localhost:22000 http://localhost:8545"
-
 TX_CRAFTER=tx-crafter
 TX_NONCE=tx-nonce
 TX_SIGNER=tx-signer
@@ -22,32 +19,6 @@ WALLET_GENERATOR=wallet-generator
 echo "Creating topics if not exist..."
 RETRY=10
 TOPICS=($TX_CRAFTER $TX_NONCE $TX_SIGNER $TX_SENDER $TX_DECODED $TX_RECOVER $WALLET_GENERATED $WALLET_GENERATOR)
-
-for ENDPOINT in ${ETH_CLIENT_URL_FROM_LOCALHOST-http://localhost:8545}
-do
-    # Retry 10 times if could not call blockchain endpoint
-    for i in $(seq 1 1 $RETRY)
-    do
-        CHAINID=$(curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_version","params":[],"id":1}' $ENDPOINT | jq .result | jq tonumber)
-        # Check if CHAINID is an number
-        re='^[0-9]+$'
-        if [[ $CHAINID =~ $re ]] ; then
-            break
-        fi
-        echo "
-=======================================================================
-Attempt $i/$RETRY (retry in 2 seconds) - could not retrieve chain id from $ENDPOINT
-=======================================================================
-        "
-        if [ $i = $RETRY ]; then
-            echo "Stopping ..."
-            exit
-        fi
-        # Sleep 2 seconds if not succeded
-        sleep 2
-    done
-    TOPICS+=($TX_DECODER-$CHAINID)
-done
 
 for NAME in ${TOPICS[@]}
 do
