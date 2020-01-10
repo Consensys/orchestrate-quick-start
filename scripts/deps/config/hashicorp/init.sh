2ac56bcf604d79d3e9cc8d0537291551fbf2d8fc
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2020-01-10 18:21:28 +0100

    clean docstring

--- scripts/deps/config/hashicorp/init.sh
+++ scripts/deps/config/hashicorp/init.sh
@@ -1,21 +1,20 @@
 # Init Vault
 curl --request POST --data '{"secret_shares": 1, "secret_threshold": 1}' ${VAULT_ADDR}/v1/sys/init > init.json
 
-# UNSEAL the vault
-export VAULT_UNSEAL_KEY=$(cat init.json | jq .keys | jq .[0])
-curl --request POST --data '{"key": '${VAULT_UNSEAL_KEY}'}' ${VAULT_ADDR}/v1/sys/unseal
+# Retrieve root token and unseal key
+root_token=$(cat init.json | jq .root_token | tr -d '"')
+unseal_key=$(cat init.json | jq .keys | jq .[0])
+rm init.json
 
-# Set the ROOT_TOKEN as environment variable
-token=$(cat init.json | jq .root_token | tr -d '"')
+# Unseal Vault
+curl --request POST --data '{"key": '${unseal_key}'}' ${VAULT_ADDR}/v1/sys/unseal
 
 # Enable secret engine
 curl --header "X-Vault-Token: ${token}" --request POST \
         --data '{"type": "kv-v2", "config": {"force_no_cache": true} }' \
     ${VAULT_ADDR}/v1/sys/mounts/secret
 
-rm init.json
-
-# Store root Token in a file (/vault/token should be shared volume with the tx-signer)
+# Store root token in a file so it can be shared with other services throug volume
 mkdir -p /vault/token
 touch /vault/token/.vault-token
 echo $token > /vault/token/.vault-token
