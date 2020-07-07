997032a27aece499ab1fd1d6c7e8ad517f8662f3
ggarri <ggarri@gmail.com>
2020-07-07 14:44:32 +0200

    Integrate SDK chain-registry wrapper

---
+++ src/register-chain/app.ts
@@ -0,0 +1,18 @@
+// tslint:disable: no-console
+
+import { ChainRegistry } from 'pegasys-orchestrate'
+
+export const start = async () => {
+  try {
+    const chainRegistry = new ChainRegistry(process.env.CHAIN_REGISTRY_HOST!)
+
+    const chain = await chainRegistry.registerChain({
+      name: process.env.CHAIN!,
+      urls: [process.env.NETWORK_ENDPOINT!]
+    })
+
+    console.log(chain)
+  } catch (error) {
+    console.error(error)
+  }
+}
